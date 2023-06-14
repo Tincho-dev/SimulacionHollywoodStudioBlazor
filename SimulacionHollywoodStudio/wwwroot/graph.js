ba77eb16160c550c3923fe9c71f960b882669cb4
Martin Lopez Rubio <Martin.LopezRubio@alu.frt.utn.edu.ar>
2023-06-14 13:22:20 -0300

    graph.js, funcion mostrada en pagina

---
+++ SimulacionHollywoodStudio/wwwroot/graph.js
@@ -0,0 +1,34 @@
+﻿window.plotFunction = () => {
+    // Crear un array para tus datos x e y
+    let x = [];
+    let y = [];
+
+    for (let i = 0; i <= 10; i++) {
+        x.push(i);
+        y.push(161 * i);
+    }
+
+    // Obtener el elemento canvas del DOM
+    let ctx = document.getElementById('myChart').getContext('2d');
+
+    // Crear el gráfico
+    let myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: x, // eje X
+            datasets: [{
+                label: 'y = 161*x',
+                data: y, // eje Y
+                borderColor: 'rgba(255,99,132,1)', // color de la línea
+                borderWidth: 1 // ancho de la línea
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
