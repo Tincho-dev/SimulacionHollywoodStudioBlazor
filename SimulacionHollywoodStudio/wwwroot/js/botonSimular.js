2e63747cf6a54e86c875bd145b65ba0f20e3e94f
Augusto Paz <augustoxx2015@gmail.com>
2023-06-22 19:52:51 -0300

    contenedor de estado y respuesta

--- SimulacionHollywoodStudio/wwwroot/js/botonSimular.js
+++ SimulacionHollywoodStudio/wwwroot/js/botonSimular.js
@@ -1,23 +1,27 @@
 
 
-document.addEventListener('DOMContentLoaded', function () {
+setTimeout(function () {
     // Tu código JavaScript aquí
-    //var botonSimular = document.getElementById('botondeSimular');
-    const botonSimular = document.querySelector('.botondeSimular');
-    console.log(botonSimular)
-    //var contenedorRespuesta = document.getElementById('contenedorRespuesta');
-    const contenedorRespuesta = document.querySelector('.contenedorRespuesta');
+    // Se ejecutará después de 2 segundos
+    document.addEventListener('DOMContentLoaded', function () {
+        // Tu código JavaScript aquí
+        //var botonSimular = document.getElementById('botondeSimular');
+        const botonSimular = document.querySelector('.botondeSimular');
+        console.log(botonSimular)
+        //var contenedorRespuesta = document.getElementById('contenedorRespuesta');
+        const contenedorRespuesta = document.querySelector('.contenedorRespuesta');
 
-    botonSimular.addEventListener('click', function () {
-        if (contenedorRespuesta.style.display === 'none') {
-            console.log("se muestra el contenedor")
-            contenedorRespuesta.style.display = 'block'; // Muestra el div
-        } else {
-            console.log("No se muestra el contenedor")
-            contenedorRespuesta.style.display = 'none'; // Oculta el div
-        }
+        //botonSimular.addEventListener('click', function () {
+        //    if (contenedorRespuesta.style.display === 'none') {
+        //        console.log("se muestra el contenedor")
+        //        contenedorRespuesta.style.display = 'block'; // Muestra el div
+        //    } else {
+        //        console.log("No se muestra el contenedor")
+        //        contenedorRespuesta.style.display = 'none'; // Oculta el div
+        //    }
+        //});
     });
-});
+}, 2000); // El valor 2000 representa los milisegundos (2 segundos)
 
 
 
