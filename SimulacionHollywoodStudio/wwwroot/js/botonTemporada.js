51fc436c70f6a1e94c709cb463c6792616b60c61
Martin Lopez Rubio <Martin.LopezRubio@alu.frt.utn.edu.ar>
2023-07-07 15:49:07 -0300

    eliminado de imports innecesarios

--- SimulacionHollywoodStudio/wwwroot/js/botonTemporada.js
+++
@@ -1,7 +0,0 @@
-function highlightButton(button) {
-    var buttons = document.querySelectorAll(".btn-group button");
-    buttons.forEach(function (btn) {
-        btn.classList.remove("highlighted");
-    });
-    button.classList.add("highlighted");
-}
